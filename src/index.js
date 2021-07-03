200029c0147d9662280406827b52ef0d43f72e67
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-07-03 15:40:13 -0600

    Add method to retrieve info

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import './style.css';
+//import generateLanding from './modules/landing';
+
+const apiKey = "2ea2600b2a9f8c39615a9c6cc9ecc342";
+let units="celsius";
+
+const cityInput = document.getElementById('input');
+
+
+cityInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      let city = cityInput.value;
+       getWeather(city);
+    }
+});
+
+
+
+
+async function getWeather(city) {
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+   const response = await fetch(url, {mode: 'cors'});
+   const data = await response.json();
+   console.log(data);
+   draw(data);
+}
+
+
+
+function draw(data){
+  const country=data.sys.country;
+  const city=data.name;
+  const temp=Math.round(data.main.temp);
+  const weather=data.weather[0]["description"];
+
+  console.log(country);
+  console.log(city);
+  console.log(temp);
+  console.log(weather);
+
+
+}
