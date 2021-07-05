9e2120ac8c52e40a9c67fe088d7e211d9bafaced
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-07-04 19:59:39 -0600

    Add styling and error message

--- src/index.js
+++ src/index.js
@@ -21,52 +21,68 @@ async function getWeather(city) {
    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
    const response = await fetch(url, {mode: 'cors'});
    const data = await response.json();
+   console.log(data);
    draw(data);
 }
 
 
 
 function draw(data){
-  const data_country=data.sys.country;
-  const data_city=data.name;
-  const data_temp=Math.round(data.main.temp);
-  const data_weather=data.weather[0]["description"];
+  const result_section = document.getElementById('result_section');
+  if (data.cod=="404"){
+    const location = document.createElement('div');
+    const country = document.createElement('div');
+    location.setAttribute('class', 'location');
+    country.setAttribute('class', 'country');
+    location.appendChild(country);
+    result_section.appendChild(location);
+    country.innerHTML = "Select a valid location";
 
-
-  const content = document.getElementById('content');
-  const result_section = document.createElement('div');
-  const location = document.createElement('div');
-  const weat = document.createElement('div');
-  const tem = document.createElement('div');
-  const desc = document.createElement('div');
-  const country = document.createElement('div');
-  const city = document.createElement('div');
-
-  result_section.setAttribute('class', 'result_section');
-  location.setAttribute('class', 'location');
-  weat.setAttribute('class', 'weather');
-  tem.setAttribute('class', 'temp');
-  desc.setAttribute('class', 'desc');
-  country.setAttribute('class', 'country');
-  city.setAttribute('class', 'city');
-
-  location.appendChild(country);
-  location.appendChild(city);
-
-  weat.appendChild(tem);
-  weat.appendChild(desc);
-
-  result_section.appendChild(location);
-  result_section.appendChild(weat);
-
-  content.appendChild(result_section);
+  }else {
+    const data_country=data.sys.country;
+    const data_city=data.name;
+    const data_temp=Math.round(data.main.temp);
+    const data_weather=data.weather[0]["description"];
 
 
 
-  tem.innerHTML = data_temp;
-  country.innerHTML = data_country;
-  city.innerHTML = data_city;
-  desc.innerHTML = data_weather;
+
+    result_section.innerHTML="";
+
+    const location = document.createElement('div');
+    const weat = document.createElement('div');
+    const tem = document.createElement('div');
+    const desc = document.createElement('div');
+    const country = document.createElement('div');
+    const city = document.createElement('div');
+
+
+    location.setAttribute('class', 'location');
+    weat.setAttribute('class', 'weather');
+    tem.setAttribute('class', 'temp');
+    desc.setAttribute('class', 'desc');
+    country.setAttribute('class', 'country');
+    city.setAttribute('class', 'city');
+
+    location.appendChild(country);
+    location.appendChild(city);
+
+    weat.appendChild(tem);
+    weat.appendChild(desc);
+
+    result_section.appendChild(location);
+    result_section.appendChild(weat);
+
+
+
+
+    tem.innerHTML = data_temp+"°"+"C";
+    country.innerHTML = data_country+" ";
+    city.innerHTML = data_city;
+    desc.innerHTML = data_weather.charAt(0).toUpperCase() + data_weather.slice(1);;
+
+  }
+
 
 
 }
